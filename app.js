1a4d3e1f972a9d4544beb19da6dce40c8335b051
as777ms <anjasajfullin@gmail.com>
2024-11-05 09:16:36 +0500

    first upld

---
+++ app.js
@@ -0,0 +1,77 @@
+// function convert(a){
+//     let array = a.map((el, arr)=>{
+//         return el.toUpperCase()
+//     })
+//     return array
+// }
+// console.log(convert(["Hello", "World"]))
+
+
+//2
+// function calculate(arr){
+//     let array = arr.map((el, arr)=>{
+//         if(typeof el === 'number'){
+//             return el+1
+//         }
+//         else{
+//             return el
+//         }
+//     }) 
+//     return array
+// }
+// console.log(calculate(["Anisa", 10]))
+
+
+//2
+// function calculate(arr){
+//     return arr.map((el)=>(
+//     {
+//         name: el.name,
+//         age: el.age+1
+//     }
+//     ))
+// }
+// console.log(calculate([{name: "Anisa", age: 10}, {name: "Human", age: 1000}]))
+
+
+//3
+// function numbersSquer(arr){
+//     return arr.map((el)=>{
+//         return el*el
+//     })
+// }
+// console.log(numbersSquer([1,2,3,4]))
+
+
+//4
+// function logelEments(arr){
+//     return arr.forEach((el)=>{
+//          console.log(el)
+//      })
+// }
+// console.log(logelEments([1,2,3,4]))
+
+
+//5
+// function summ(arr){
+//     let cnt = 0
+//     arr.forEach((el)=>{
+//         cnt += el
+//     })
+//     return cnt
+// }
+// console.log(summ([1,2,3,4,5]))
+
+//6
+// function priceProduct(arr){
+//     let cnt = 0
+//     arr.forEach((el)=>{
+//         cnt += el.price *1.10
+//         console.log(cnt)
+//     })
+//     return cnt
+// }
+// (priceProduct([{ name: "Product A", price: 100 },{ name: "Product B", price: 200 },{ name: "Product C", price: 300 }]))
+
+
+//7
